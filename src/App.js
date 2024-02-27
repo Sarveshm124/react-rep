a07d110f0b64e575a959ec2858f6c4cf7f2e3658
Sarvesh Mahajan <skmahajan265@gmail.com>
2024-02-27 11:03:03 +0530

    first react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <img src="https://tse1.mm.bing.net/th?id=OIP.zCPYmuGuI5N9X1LixT7ZDwHaEA&pid=Api&P=0&h=180"
+     alt="not found"/>
+    </div>
+  );
+}
+
+export default App;
